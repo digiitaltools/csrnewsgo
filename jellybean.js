4e88770f29e42dff273f9691e7d6bc1c769d0c86
Digiitaltools <ahmad.byu95@gmail.com>
2026-05-09 21:41:46 +0700

    Update jellybean.js

--- jellybean.js
+++ jellybean.js
@@ -3,7 +3,7 @@
 		API_URL: "https://newsgo.space",
 		API_KEY: "berbahagia", 
 		DOMAIN: window.location.origin,
-		DATABASE_NAME: "database" 
+		DATABASE_NAME: "databases" 
 	};
 
     const memoryCache = new Map();
